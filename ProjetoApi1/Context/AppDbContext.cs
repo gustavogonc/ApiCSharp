70f78ca5820bb742c9fc82728d0285df8f13b8cf
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2023-03-05 18:14:58 -0300

    Api finalizada

---
+++ ProjetoApi1/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoApi1.Models;
+
+namespace ProjetoApi1.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Categoria>? Categorias { get; set; }
+        public DbSet<Produto>? Produtos { get; set; }
+    }
+}
---
+++ ProjetoApi1/Controllers/CategoriasController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjetoApi1.Context;
+using ProjetoApi1.Models;
+
+namespace ProjetoApi1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("produtos")]
+        public ActionResult<IEnumerable<Categoria>> GetCategoriasProdutos()
+        {
+            //return _context.Categorias.Include(p => p.Produtos).ToList();
+            return _context.Categorias.Include(p => p.Produtos).Where(c => c.CategoriaId <= 5).ToList();
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            try
+            {
+                var categoria = _context.Categorias.AsNoTracking().ToList();
+                if (categoria is null)
+                {
+                    return NotFound("Produtos não encontrados...");
+                }
+                return categoria;
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, 
+                    "Ocorreu um problema ao tratar a sua solicitação.");
+            }
+        }
+
+        [HttpGet("{id:int}", Name = "ObterCategoria")]
+        public ActionResult<Categoria> Get(int id)
+        {
+            try
+            {
+                var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
+                if (categoria is null)
+                {
+                    return NotFound($"Categoria com id = {id} não encontrada...");
+                }
+                return Ok(categoria);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Ocorreu um problema ao tratar a sua solicitação.");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Post(Categoria categoria)
+        {
+            if (categoria == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Categorias.Add(categoria);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("Obtercategoria", new { id = categoria.CategoriaId }, categoria);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Categoria categoria)
+        {
+            if (id != categoria.CategoriaId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(categoria).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(categoria);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult<Categoria> Delete(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
+            //var produto = _context.Produtos.Find(id);
+            if (categoria == null)
+            {
+                return NotFound($"Categoria com id={id} não localizada...");
+            }
+            _context.Categorias.Remove(categoria);
+            _context.SaveChanges();
+
+            return Ok(categoria);
+        }
+    }
+}
